2416bea22208d546f5b1213770c057512084b920
Cory Zue <czue@dimagi.com>
2013-03-13 16:01:57 -0400

    no need to override __init__

--- djangocouch/resource.py
+++ djangocouch/resource.py
@@ -4,8 +4,6 @@ import logging
 class CouchdbResouceHack(CouchdbResource):
     # this is just a proof of concept to test that overriding the
     # resource actually works
-    def __init__(self, *args, **kwargs):
-        return super(CouchdbResouceHack, self).__init__(*args, **kwargs)
 
     def request(self, *args, **kwargs):
         logging.debug('made a request!')
